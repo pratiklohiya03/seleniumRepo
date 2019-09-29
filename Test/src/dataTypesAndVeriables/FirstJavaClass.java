30f7c6b49d2ad4bad62d24621301a25dff61d25c
Pratik Lohiya <pratiklohiya03@gmail.com>
2019-09-29 11:21:18 +0530

    This file contain main method for project start

---
+++ Test/src/dataTypesAndVeriables/FirstJavaClass.java
@@ -0,0 +1,15 @@
+package dataTypesAndVeriables;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("Hellow World!!! Welcome to Selenium");
+		
+
+	}
+
+}
